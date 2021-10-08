9f14a85697668c84cbf261c6ec85fd242a4b4438
Akash <akashjais929@gmail.com>
2021-10-08 14:59:11 +0530

    Added snake object

--- snake.js
+++ snake.js
@@ -1,79 +1,40 @@
+console.log("snake");
 
 
-
-
-
-
-
-
-// pen.fillRect(20,20,50,50);//drawing a filled rectanglle at 20,20 with height=50 and width=50
-
-
-//Operations inside the init function are done only once
+//operations inside the init function are executed only once
 function init() {
-    canvas = document.getElementById("mycanvas");
-
-    //variables with not any data type are global variables
-
-    W = canvas.width = 500;
-    H = canvas.height = 500;
-
-    //canvas is used to draw graphics
-
+    //first fetch the canvas element
+    var canvas = document.getElementById("mycanvas");
+    W = H = canvas.width = canvas.height = 1000;
     pen = canvas.getContext('2d');
+    cellSize = 66;
+    //setting the snake object
 
-    //by pen we can draw something on the canvas
+    snake = {
+        init_length: 5,
+        color: "blue",
+        cells: [],
+        direction: "right",
 
-    rect = {
-        x: 20,
-        y: 20,
-        w: 40,
-        h: 40,
-        speed: 10
-    }
+        createSnake: function () {
+            for (var i = this.init_length; i > 0; i--) {
+                this.cells.push({ x: i, y: 0 });
+            }
+        },
+
+        drawSnake: function () {
+            for (var i = 0; i < this.cells.length; i++) {
+                pen.fillRect(this.cells[i].x * cellSize, this.cells[i].y * cellSize, cellSize - 2, cellSize - 2);
+            }
+        }
+    };
+
+
+    snake.createSnake();
+    snake.drawSnake();
+
+
 }
 
-function draw() {
-    // console.log("Inside draw");
-    pen.clearRect(0, 0, W, H);
-    //We clear the canvas before drawing the next rectangle
-    pen.fillRect(rect.x, rect.y, rect.h, rect.w);
-    pen.fillStyle = "blue";//drawing anything will get color blue
-
-}
-
-function update() {
-    // console.log("Inside update");
-    rect.x += rect.speed;
-    if (rect.x > W - rect.w || rect.x < 0) {
-        rect.speed *= -1;
-    }
-    //if the x co-ordinate of the snake goes outside the boundaries it will set the speed to -1 to change direction
-}
-
-function gameLoop() {
-    console.log("Inside gameLoop");
-    draw();
-    update();
-}
 
 init();
-var f = setInterval(() => {
-    gameLoop();
-}, 100);
-
-
-
-
-
-/*
- * Game Loop=>
- * 1. Intial stage =>
- * 2.Draw <=> Update =>
- * 4.This will go on until game over
- */
-
-
-
-
-
