30c86b0fbf9bdefc09c4567e88469ac42d6fe402
Akash <akashjais929@gmail.com>
2021-10-08 18:06:06 +0530

    Added basic movement to the snake

--- snake.js
+++ snake.js
@@ -24,17 +24,54 @@ function init() {
 
         drawSnake: function () {
             for (var i = 0; i < this.cells.length; i++) {
+                if(i==0){
+                    pen.fillStyle="red";
+                }
+                else{
+                    pen.fillStyle="blue";
+                }
                 pen.fillRect(this.cells[i].x * cellSize, this.cells[i].y * cellSize, cellSize - 2, cellSize - 2);
             }
+        },
+
+        updateSnake: function(){
+            //Pop the last cell from the snake and then add a new element at the start of the snake
+            //In this way the snake will gets its length incresed by 1
+            this.cells.pop();
+            var headX= this.cells[0].x;
+            var headY= this.cells[0].y;
+
+            var X= headX+1;
+            var Y= headY;
+            this.cells.unshift({x:X,y:Y});
         }
     };
 
 
     snake.createSnake();
-    snake.drawSnake();
 
 
 }
 
 
+function draw(){
+    pen.clearRect(0,0,W,H);
+    snake.drawSnake();
+}
+
+function update(){
+    console.log("Update Snake");
+    snake.updateSnake();
+}
+
+
+
+function gameLoop(){
+    draw();
+    update();
+}
 init();
+
+setInterval(() => {
+    gameLoop();
+}, 100);
